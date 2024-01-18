17c200843734a489064561248c49c877d4696c7a
SilvioOddo <silvioscuolaitts@gmail.com>
2024-01-18 17:51:35 +0100

    first commit

---
+++ src/gestionefile/GestioneFile.java
@@ -0,0 +1,27 @@
+package gestionefile;
+
+/**
+ *
+ * @author SO
+ * @version 18/01/23
+ */
+
+public class GestioneFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //1)LETTURA
+        Lettore lettore = new Lettore("user.json");
+        lettore.start();
+        //2)ELABORAZIONE
+        
+        //3) SCRITTURA
+        Scrittore scrittore = new Scrittore("output.csv");
+        Thread threadScrittore = new Thread(scrittore);
+        threadScrittore.start();
+    }
+    
+}
